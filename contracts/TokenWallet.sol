43158da18062af7124ba89759e50f7fc6aa34f8a
ldmonster <36456348+ldmonster@users.noreply.github.com>
2022-05-23 17:36:20 +0300

    Update TokenWallet.sol

--- contracts/TokenWallet.sol
+++ contracts/TokenWallet.sol
@@ -7,15 +7,12 @@ interface IERC20 {
     function balanceOf(address account) external view returns (uint256);
     function transfer(address recipient, uint256 amount) external returns (bool);
     function allowance(address owner, address spender) external view returns (uint256);
-    function approve(address spender, uint256 amount) external returns (bool);
     function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
 }
 
 contract TokenWallet is Context{
 
     address private tokenAddress;
-
-        event FailedTransaction(string revertState);
 
     constructor(address _address) {
         tokenAddress = _address;
